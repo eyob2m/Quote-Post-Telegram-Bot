0df0f3089b1bfc159cb43677c7985b3d21d9dadc
eyob2m <105048361+eyob2m@users.noreply.github.com>
2022-10-06 10:43:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,17 +7,19 @@ import time
 API_KEY = '5758248073:AAHhRY67sCd7wDIbwTtJo6YMRNQFQfiE9dU'
 bot = telebot.TeleBot(API_KEY)
 channel = "@Quotes_P30"
-url = requests.get('https://story-shack-cdn-v2.glitch.me/generators/quote-generator')
 
-e = url.json()
-j = e['data']['name']
 def post():
+  url = requests.get('https://story-shack-cdn-v2.glitch.me/generators/quote-generator')
+
+  e = url.json()
+
+  j = e['data']['name']
   bot.send_message(channel,j +'\n\U0001f31a | @Quotes_P30')
 
 
 
 def p1():
-    schedule.every(30).minutes.until("19:00").do(post)
+    schedule.every(31).minutes.until("19:00").do(post)
 schedule.every().day.at("04:00").do(p1)
 while True:
     schedule.run_pending()
