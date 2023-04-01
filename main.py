258b937817cacb4c1623b7fa91e09519463125c8
eyob2m <105048361+eyob2m@users.noreply.github.com>
2023-04-01 10:20:17 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from PIL import Image,ImageFont,ImageDraw
 image = Image.open("demo.jpg")
 
-font = ImageFont.truetype("arial.ttf", 15)
+font = ImageFont.load_default()
 draw = ImageDraw.Draw(image)
 
 
