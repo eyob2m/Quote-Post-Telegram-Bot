ddb95df5f188adedd44aa4a6db6f2825be581e2f
eyob2m <105048361+eyob2m@users.noreply.github.com>
2023-04-04 06:14:32 -0700

    Update main.py

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ def post():
     bot.send_photo(channel2, image, caption=j + '\n\U0001f31a | @Quotes4_life') 
     
 def p1():
-    schedule.every(120).minutes.until("19:00").do(post)
+    schedule.every(240).minutes.until("19:00").do(post)
 schedule.every().day.at("04:00").do(p1)
 while True:
     schedule.run_pending()
